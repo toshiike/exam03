1670615d103c9348e6546f8856fd24c120140355
Toshi <toshiaki.ikeda.study@gmail.com>
2017-09-23 15:12:59 +0900

    “modify”

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,6 +9,7 @@ CarrierWave.configure do |config|
 
   config.fog_public     = true
   config.fog_attributes = {'Cache-Control' => 'public, max-age=86400'}
+  config.cache_storage = :fog
 
   case Rails.env
     when 'production'
