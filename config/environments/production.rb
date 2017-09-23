8e0a436c0f8ad40b43163df45151f0e482e048d1
Toshi <toshiaki.ikeda.study@gmail.com>
2017-09-23 10:36:36 +0900

    “modify”

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'stark-dusk-84071.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: '54.64.79.87' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings =
 {
